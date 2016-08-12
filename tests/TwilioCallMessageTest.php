177b021ee43fc493b93d3ef3841f22bcc97209e6
Gregorio Hernandez Caso <gregoriohc@gmail.com>
2016-08-12 16:47:44 +0200

    Add some basic message tests

---
+++ tests/TwilioCallMessageTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace NotificationChannels\Twilio\Test;
+
+use Illuminate\Support\Arr;
+use NotificationChannels\Twilio\TwilioCallMessage;
+use PHPUnit_Framework_TestCase;
+
+class TwilioCallMessageTest extends PHPUnit_Framework_TestCase
+{
+    /** @var \NotificationChannels\Twilio\TwilioCallMessage */
+    protected $message;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->message = new TwilioCallMessage();
+    }
+
+    /** @test */
+    public function it_can_accept_an_url_when_constructing_a_message()
+    {
+        $message = new TwilioCallMessage('http://example.com');
+
+        $this->assertEquals('http://example.com', $message->url);
+    }
+
+    /** @test */
+    public function it_provides_a_create_method()
+    {
+        $message = TwilioCallMessage::create('http://example.com');
+
+        $this->assertEquals('http://example.com', $message->url);
+    }
+
+    /** @test */
+    public function it_can_set_the_url()
+    {
+        $this->message->url('http://example.com');
+
+        $this->assertEquals('http://example.com', $this->message->url);
+    }
+
+    /** @test */
+    public function it_can_set_the_from()
+    {
+        $this->message->from('+1234567890');
+
+        $this->assertEquals('+1234567890', $this->message->from);
+    }
+}
---
+++ tests/TwilioSmsMessageTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace NotificationChannels\Twilio\Test;
+
+use Illuminate\Support\Arr;
+use NotificationChannels\Twilio\TwilioSmsMessage;
+use PHPUnit_Framework_TestCase;
+
+class TwilioSmsMessageTest extends PHPUnit_Framework_TestCase
+{
+    /** @var \NotificationChannels\Twilio\TwilioSmsMessage */
+    protected $message;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->message = new TwilioSmsMessage();
+    }
+
+    /** @test */
+    public function it_can_accept_a_message_when_constructing_a_message()
+    {
+        $message = new TwilioSmsMessage('myMessage');
+
+        $this->assertEquals('myMessage', $message->content);
+    }
+
+    /** @test */
+    public function it_provides_a_create_method()
+    {
+        $message = TwilioSmsMessage::create('myMessage');
+
+        $this->assertEquals('myMessage', $message->content);
+    }
+
+    /** @test */
+    public function it_can_set_the_content()
+    {
+        $this->message->content('myMessage');
+
+        $this->assertEquals('myMessage', $this->message->content);
+    }
+
+    /** @test */
+    public function it_can_set_the_from()
+    {
+        $this->message->from('+1234567890');
+
+        $this->assertEquals('+1234567890', $this->message->from);
+    }
+}
